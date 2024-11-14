7c2558a74a0a85f47f5d67e291a03826087fe320
Anindito Rahardyaksa <ditorahard@gmail.com>
2024-11-14 23:36:57 +0700

    Add keystatic config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
 
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), markdoc(), keystatic()],
   vite: {
     ssr: {
       // Example: Force a broken package to skip SSR processing, if needed
       external: ['legion-react'],
     }
-  }
+  },
+  output: "hybrid"
 });

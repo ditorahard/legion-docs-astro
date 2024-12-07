33f3f756fc4413086a163e840cc83b52db565181
Anindito Rahardyaksa <ditorahard@gmail.com>
2024-12-07 16:15:33 +0700

    change keystatic config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -6,24 +6,63 @@ export default config({
   },
 
   collections: {
-    posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
-      schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({
-          label: 'Content',
-          options: {
-            image:{
-                publicPath:'src/content/posts/',
-                directory:'src/content/posts/'
-            }
-          }
-        }),
-      },
-    }),
+    // Commented for later use
+    // developer: collection({
+    //   label: 'Developer',
+    //   path: 'src/content/developer/**/**/*',
+    //   slugField: 'component',
+    //   format: { contentField: 'content' },
+    //   schema: {
+    //     title: fields.text({ label: 'Title' }),
+    //     description: fields.text({ label: 'Description' }),
+    //     library: fields.select({
+    //       label: 'Library',
+    //       options: [
+    //         { label: 'React', value: 'react' },
+    //         { label: 'Vue', value: 'vue' },
+    //       ],
+    //       defaultValue: 'react',
+    //     }),
+    //     category: fields.select({
+    //       label: 'Category',
+    //       options: [
+    //         { label: 'Components', value: 'components' },
+    //         { label: 'Getting Started', value: 'getting-started' },
+    //       ],
+    //       defaultValue: 'components',
+    //     }),
+    //     component: fields.slug({ 
+    //       name: { label: 'Component Name' },
+    //     }),
+    //     content: fields.markdoc({
+    //       label: 'Content',
+    //       options: {
+    //         image: {
+    //           publicPath: '/src/content/developer',
+    //           directory: 'src/content/developer'
+    //         }
+    //       }
+    //     }),
+    //   },
+    // }),
+    // posts: collection({
+    //   label: 'Posts',
+    //   slugField: 'title',
+    //   path: 'src/content/posts/*',
+    //   format: { contentField: 'content' },
+    //   schema: {
+    //     title: fields.slug({ name: { label: 'Title' } }),
+    //     content: fields.markdoc({
+    //       label: 'Content',
+    //       options: {
+    //         image:{
+    //             publicPath:'src/content/posts/',
+    //             directory:'src/content/posts/'
+    //         }
+    //       }
+    //     }),
+    //   },
+    // }),
     designerFoundations: collection({
       label: 'Designer - Foundations',
       slugField: 'title',
@@ -49,6 +88,7 @@ export default config({
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
+        
         content: fields.markdoc({
           label: 'Content',
           options: {
@@ -62,11 +102,12 @@ export default config({
     }),
     reactGettingStarted: collection({
       label: 'React - Getting Started',
-      slugField: 'component',
+      slugField: 'title',
       path: 'src/content/developer/react/getting-started/*',
       format: { contentField: 'content' },
       schema: {
-        component: fields.slug({ name: { label: 'Component (React)' } }),
+        title: fields.slug({ name: { label: 'Title' } }),
+        description: fields.text({ label: 'Description' }),
         content: fields.markdoc({
           label: 'Content',
           options: {
@@ -86,6 +127,7 @@ export default config({
       format: { contentField: 'content' },
       schema: {
         component: fields.slug({ name: { label: 'Component (React)' } }),
+        description: fields.text({ label: 'Description' }),
         content: fields.markdoc({
           label: 'Content',
           options: {
@@ -99,11 +141,12 @@ export default config({
     }),
     reactPatterns: collection({
       label: 'React - Patterns',
-      slugField: 'component',
+      slugField: 'pattern',
       path: 'src/content/developer/react/patterns/*',
       format: { contentField: 'content' },
       schema: {
-        component: fields.slug({ name: { label: 'Component (React)' } }),
+        pattern: fields.slug({ name: { label: 'Pattern (React)' } }),
+        description: fields.text({ label: 'Description' }),
         content: fields.markdoc({
           label: 'Content',
           options: {
@@ -115,5 +158,26 @@ export default config({
         }),
       },
     }),
+    vueGettingStarted: collection({
+      label: 'Vue Getting Started',
+      path: 'src/content/developer/vue/getting-started/*',
+      slugField: 'title',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        description: fields.text({ label: 'Description' }),
+        content: fields.markdoc({
+          label: 'Content',
+          options: {
+            image: {
+              publicPath: '/src/content/developer/vue/getting-started',
+              directory: 'src/content/developer/vue/getting-started'
+            }
+          }
+        }),
+      },
+    }),
+    
+    
   },
 });

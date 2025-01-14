5fe96d0604219848718d257a5d503dfcae813a2f
Anindito Rahardyaksa <ditorahard@gmail.com>
2025-01-14 10:39:19 +0700

    favicon and collection console log

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,6 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
+import { glob, file } from 'astro/loaders'; 
 // 2. Define your collection(s)
 const postsCollection = defineCollection({
     
@@ -10,28 +11,33 @@ const reactGettingStarted = defineCollection({
 });
 
 const reactComponents = defineCollection({
+  loader: glob({ 
+    pattern: "**/*.mdoc",
+    base: "./src/content/developer/react/components" 
+  }),
   
 });
 
 
-// const developer = defineCollection({
-//   type: 'content',
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string().optional(),
-//     library: z.string(),
-//     category: z.string(),
-//     component: z.string(),
-//   }),
-// });
+const developer = defineCollection({
+  type: 'content',
+  // schema: z.object({
+  //   title: z.string(),
+  //   description: z.string().optional(),
+  //   library: z.string(),
+  //   category: z.string(),
+  //   component: z.string(),
+  // }),
+});
+
+const designer = defineCollection({
+  type: 'content',
+});
 
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'posts': postsCollection,
-  'react-getting-started': reactGettingStarted,
-  'react-components': reactComponents,
-  // 'developer': developer,
+  postsCollection, reactGettingStarted, developer, designer
 
 };
